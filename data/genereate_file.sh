d2dfeb7171d2dffe0152f54718266ff6ab9cb56d
technogleb <sinyakov@phystech.edu>
2019-07-25 22:11:50 +0300

    Modify iteration number in generate_file.sh to finite number
    * also change target to simple function, becase lambdas are not pickable
      (multiprocessing requirement)

--- data/genereate_file.sh
+++ data/genereate_file.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-for i in {1..100000}; do cat file.txt file.txt > file2.txt && mv file2.txt file.txt; done
+for i in {1..30}; do cat file.txt file.txt > file2.txt && mv file2.txt file.txt; done
